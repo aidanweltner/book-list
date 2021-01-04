91b14fa0bba5228aa74b2133a90f0be0b12bfdb7
aidanweltner <aidan.weltner@gmail.com>
2021-01-04 14:48:54 -0700

    added main layout & tailwind config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,18 +17,18 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <div class="bg-yellow-100">
+            {{-- @include('layouts.navigation') --}}
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            {{-- <header class="bg-yellow-700 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
-            </header>
+            </header> --}}
 
             <!-- Page Content -->
-            <main>
+            <main class="max-w-screen-lg mx-auto flex items-center min-h-screen sm:px-6 lg:px-8">
                 {{ $slot }}
             </main>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+            colors: {
+                gray: colors.warmGray,
+                yellow: colors.amber,
+            }
         },
     },
 
