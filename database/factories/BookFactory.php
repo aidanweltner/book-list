bb2ab508ae158b656129fca7c7f47e9b2acdf0ff
aidanweltner <aidan.weltner@gmail.com>
2021-10-30 14:55:16 -0500

    Updated book create error

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -22,7 +22,7 @@ class BookFactory extends Factory
     public function definition()
     {
         return [
-            'user_id'       => 1,
+            'user_id'       => 2,
             'slug'          => $this->faker->domainWord,
             'title'         => $this->faker->sentence(),
             'author'        => $this->faker->name(),
